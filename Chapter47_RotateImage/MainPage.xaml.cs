434b13fb49a3b63ea91760681a99d54fb21b4558
Sergey <sbaidachni@gmail.com>
2017-04-21 13:17:37 -0700

    RadialControl example

---
+++ Chapter47_RotateImage/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Haptics;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Chapter47_RotateImage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RadialControllerConfiguration config;
+        RadialController controller;
+        RadialControllerMenuItem customItem;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            controller = RadialController.CreateForCurrentView();
+            controller.RotationChanged += Controller_RotationChanged;
+            controller.ButtonHolding += Controller_ButtonHolding;
+
+            controller.UseAutomaticHapticFeedback = false;
+            
+            controller.RotationResolutionInDegrees = 1;
+
+            customItem = RadialControllerMenuItem.CreateFromKnownIcon("Rotate", RadialControllerMenuKnownIcon.Ruler);
+            controller.Menu.Items.Add(customItem);
+
+            controller.Menu.SelectMenuItem(customItem);
+
+            config = RadialControllerConfiguration.GetForCurrentView();
+            config.ActiveControllerWhenMenuIsSuppressed = controller;
+            config.SetDefaultMenuItems(new[] { RadialControllerSystemMenuItemKind.Volume, RadialControllerSystemMenuItemKind.Scroll });
+
+            //uncomment it to supress the default menu
+            //config.IsMenuSuppressed = true;
+        }
+
+        private void Controller_ButtonHolding(RadialController sender, RadialControllerButtonHoldingEventArgs args)
+        {
+            if (rotationControl.Visibility == Visibility.Visible)
+                rotationControl.Visibility = Visibility.Collapsed;
+            else
+                rotationControl.Visibility = Visibility.Visible;
+        }
+
+        private void Controller_RotationChanged(RadialController sender, RadialControllerRotationChangedEventArgs args)
+        {
+            //rotateTransform.Angle += args.RotationDeltaInDegrees;
+            rotationControl.Angle += (float)args.RotationDeltaInDegrees;
+        }
+    }
+}
---
+++ Chapter47_RotateImage/UserControls/RotationControl.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Graphics.Canvas.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Chapter47_RotateImage.UserControls
+{
+    public sealed partial class RotationControl : UserControl
+    {
+        public static readonly DependencyProperty AngleProperty = DependencyProperty.Register(
+            "Angle",
+            typeof(float),
+            typeof(RotationControl),
+            new PropertyMetadata(null)
+            );
+
+        public float Angle
+        {
+            get
+            {
+                return (float)GetValue(AngleProperty);
+            }
+            set
+            {
+                SetValue(AngleProperty, value);
+                canvasControl.Invalidate();
+            }
+        }
+
+        public RotationControl()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void canvasControl_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+            int stroke = 3;
+            float width = (float)this.ActualWidth;
+            float height = (float)this.ActualHeight;
+            float radius=Math.Min(width, height)/2-2*stroke;
+            float centerX = width / 2;
+            float centerY = height / 2;
+            float lineEndX = radius * (float)Math.Cos(Math.PI * Angle / 180) + centerX;
+            float lineEndY = radius * (float)Math.Sin(Math.PI * Angle / 180) + centerY;
+
+            args.DrawingSession.DrawCircle(centerX, centerY, radius, Colors.Red,stroke);
+            args.DrawingSession.DrawLine(centerX, centerY,lineEndX , lineEndY, Colors.Green, stroke);
+            args.DrawingSession.DrawText(Angle.ToString(), centerX, centerY, Colors.Black);
+        }
+    }
+}
